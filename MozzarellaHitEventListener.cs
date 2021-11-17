e9d43bd393e51d17ebe1b908e838f6eb3ed4cca4
naelstrof <naelstrof@gmail.com>
2021-11-16 17:13:42 -0700

    Hit events are an optional addon now, added some local multicompile support.

---
+++ MozzarellaHitEventListener.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MozzarellaHitEventListener : MonoBehaviour {
+    private Mozzarella mozzarella;
+    [Range(2, 16)]
+    public int hitEventCount = 8;
+    private ComputeBuffer hitEventsBuffer;
+    private static int hitEventsID;
+    private static int clearHitEventKernel;
+    private static int hitEventCountID;
+    public struct HitEvent {
+        public Vector3 position;
+        public float volume;
+    }
+    public delegate void HitEventAction(List<HitEvent> hitEvent);
+    private List<HitEvent> hitEvents;
+    public event HitEventAction OnDepthBufferHit;
+    // Start is called before the first frame update
+    void Awake() {
+        hitEvents = new List<HitEvent>();
+        hitEventsID = Shader.PropertyToID("_HitEvents");
+        hitEventCountID = Shader.PropertyToID("_NumHitEvents");
+    }
+    void Start() {
+        mozzarella = GetComponent<Mozzarella>();
+        mozzarella.verletProcessor.EnableKeyword("HIT_EVENTS");
+        hitEventsBuffer = new ComputeBuffer(hitEventCount, sizeof(float)*4);
+        for(int i=0;i<hitEventCount;i++) {
+            hitEvents.Add(new HitEvent(){position = Vector3.zero, volume = 0f});
+        }
+        hitEventsBuffer.SetData<HitEvent>(hitEvents);
+        mozzarella.verletProcessor.SetInt(hitEventCountID, hitEventCount);
+        mozzarella.verletProcessor.SetBuffer(mozzarella.verletProcessor.FindKernel(Mozzarella.mainKernelName), hitEventsID, hitEventsBuffer);
+        clearHitEventKernel = mozzarella.verletProcessor.FindKernel("ClearHitEvents");
+        mozzarella.verletProcessor.SetBuffer(clearHitEventKernel, hitEventsID, hitEventsBuffer);
+        mozzarella.onPointsUpdated += OnPointsUpdated;
+    }
+    void OnPointsUpdated() {
+        AsyncGPUReadback.Request(hitEventsBuffer, sizeof(float)*4*hitEventCount, 0, GetHitEvents);
+    }
+    void GetHitEvents(AsyncGPUReadbackRequest request) {
+        if (!Application.isPlaying) {
+            return;
+        }
+        hitEvents.Clear();
+        foreach(var hitEvent in request.GetData<HitEvent>()) {
+            if (hitEvent.volume != 0f) {
+                hitEvents.Add(hitEvent);
+            }
+        }
+        OnDepthBufferHit?.Invoke(hitEvents);
+        mozzarella.verletProcessor.Dispatch(clearHitEventKernel, hitEventCount, 1, 1);
+    }
+    void OnDestroy() {
+        hitEventsBuffer.Dispose();
+        if (mozzarella != null) {
+            mozzarella.verletProcessor.DisableKeyword("HIT_EVENTS");
+            mozzarella.onPointsUpdated -= OnPointsUpdated;
+        }
+    }
+}
