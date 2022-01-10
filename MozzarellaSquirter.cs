9f3796f0061c888c97f2095892a473edab2ea03c
naelstrof <naelstrof@gmail.com>
2022-01-10 16:44:10 -0700

    changed how particles are spawned, they're directly sent by CPU now rather than doing weird gpu shenanigans.

--- MozzarellaSquirter.cs
+++ MozzarellaSquirter.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Mozzarella))]
-public class MozzarellaSquirter : MonoBehaviour {
+public class MozzarellaSquirter : BaseStreamer {
     [Range(0.1f,10f)]
     [SerializeField]
     private float squirtDuration = 0.5f;
-    private int currentIndex;
+    [Range(1,16)]
+    [SerializeField]
+    private int squirtCount = 1;
     [SerializeField]
     private AnimationCurve volumeCurve;
     [SerializeField]
@@ -16,28 +18,44 @@ public class MozzarellaSquirter : MonoBehaviour {
     private float velocityMultiplier = 0.1f;
     [SerializeField][Range(0f,1f)]
     private float velocityVariance = 0f;
-    Mozzarella mozzarella;
-    void Start() {
-        currentIndex = 0;
-        mozzarella = GetComponent<Mozzarella>();
-    }
-    IEnumerator Squirt(int i, float duration) {
-        float startTime = Time.time;
-        while(Time.time < startTime+duration) {
-            float t = (Time.time-startTime)/duration;
 
-            float volume = volumeCurve.Evaluate(t);
-
-            mozzarella.squirts[i] = new Mozzarella.Squirt(transform.position,
-            Vector3.up*0.025f+transform.up*velocityCurve.Evaluate(t)*velocityMultiplier+UnityEngine.Random.insideUnitSphere*velocityVariance,
-            volume,
-            mozzarella.squirts[i].index);
-            yield return null;
+    protected class SquirtStream : BaseStreamer.Stream {
+        private float startTime;
+        private float duration;
+        public SquirtStream( float startTime, float duration ) {
+            this.startTime = startTime;
+            this.duration = duration;
         }
-        mozzarella.squirts[i] = new Mozzarella.Squirt(transform.position, Vector3.zero, 0f, mozzarella.squirts[i].index);
+        public bool IsFinished() {
+            return Time.time > startTime + duration;
+        }
+        public override Mozzarella.Point CreatePoint(BaseStreamer streamer, float time) {
+            if (!(streamer is MozzarellaSquirter)) {
+                throw new UnityException("Tried to use a squirter stream on a non-squirter behavior.");
+            }
+            float t = (Time.time-startTime)/duration;
+            MozzarellaSquirter squirter = streamer as MozzarellaSquirter;
+            Vector3 velocity = Vector3.up*0.025f+squirter.transform.up*squirter.velocityCurve.Evaluate(t)*squirter.velocityMultiplier+UnityEngine.Random.insideUnitSphere*squirter.velocityVariance;
+            float volume = squirter.volumeCurve.Evaluate(t);
+            return new Mozzarella.Point() {
+                position = squirter.transform.position,
+                prevPosition = squirter.transform.position - velocity,
+                volume = volume
+            };
+        }
+    }
+    public override void Update() {
+        base.Update();
+        for(int i=streams.Count-1;i>=0;i--) {
+            SquirtStream squirter = streams[i] as SquirtStream;
+            if (squirter.IsFinished()) {
+                streams.RemoveAt(i);
+            }
+        }
     }
     public void Squirt() {
-        StartCoroutine(Squirt(currentIndex, squirtDuration));
-        currentIndex = (++currentIndex)%(mozzarella.squirts.Count-1);
+        for(int i=0;i<squirtCount;i++) {
+            streams.Add(new SquirtStream(Time.time, squirtDuration));
+        }
     }
 }
